5d3d422da03a5ed138f989dc3c4a3d749428ce31
PhaniGudipati <125386823+PhaniGudipati@users.noreply.github.com>
2023-06-07 20:10:01 +0530

    Add files via upload

---
+++ Xyz.java
@@ -0,0 +1,20 @@
+class Xyz{
+	public static void main(String args[]){
+		System.out.println( args[0]);//"10"
+		System.out.println(args[1]);
+		System.out.println(args[0]+args[1]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
